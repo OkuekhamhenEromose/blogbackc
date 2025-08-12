149c7625985bd12141f36206daad803f77bf3dcc
Eromose Charles <okuekhamhene@gmail.com>
2025-08-12 17:27:55 +0100

    updating env

--- api/settings.py
+++ api/settings.py
@@ -10,9 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
